02e90632b25c0d2fc82d85003070aca364e291b3
Jayaram <jayaramvenkatanarayanan@live.com>
2025-05-18 20:42:04 +0530

    feat: fixed version

--- getVersion.js
+++ getVersion.js
@@ -3,33 +3,43 @@ const path = require("path");
 const fs = require("fs");
 const { execSync } = require("child_process");
 
-const pkgPath = path.join(__dirname, "package.json");
-const packageJson = JSON.parse(fs.readFileSync(pkgPath, "utf8"));
+const packageJson = JSON.parse(fs.readFileSync(path.join(__dirname, "package.json"), "utf8"));
 const currentVersion = packageJson.version;
 
-const input = (process.argv[2] || "").trim().toLowerCase();
-const releaseType = ["stable", "minor"].includes(input) ? input : "beta";
+const releaseType = ["stable", "minor"].includes((process.argv[2] || "").trim().toLowerCase())
+  ? (process.argv[2] || "").trim().toLowerCase()
+  : "beta";
 console.log("🚀 ~ releaseType:", releaseType);
 
 let newVersion;
 
 if (releaseType === "beta") {
-  if (semver.prerelease(currentVersion)) {
-    newVersion = semver.inc(currentVersion, "prerelease", "beta");
+  if (semver.prerelease(packageJson.version)) {
+    /**
+     * 1.0.0-beta-1 => 1.0.0-beta.2
+     */
+    newVersion = semver.inc(packageJson.version, "prerelease", "beta");
   } else {
-    newVersion = semver.inc(currentVersion, "preminor", "beta");
+    /**
+     * 1.0.0 => 1.0.0-beta.1
+     */
+    newVersion = semver.inc(packageJson.version, "prerelease", "beta");
   }
-} else {
-  if (semver.prerelease(currentVersion)) {
-    const parsed = semver.parse(currentVersion);
-    const base = `${parsed.major}.${parsed.minor}.${parsed.patch}`;
+} else if (releaseType === "minor" || releaseType === "stable") {
+  if (semver.prerelease(packageJson.version)) {
+    /**
+     * 1.0.0-beta-1 => 1.1.0
+     * 1.1.0-beta-10 => 1.2.0
+     */
+    const parsed = semver.parse(packageJson.version);
+    removedPreVersion = `${parsed.major}.${parsed.minor}.${parsed.patch}`;
     newVersion = semver.inc(
-      base,
+      removedPreVersion,
       releaseType === "stable" ? "major" : "minor"
     );
   } else {
     newVersion = semver.inc(
-      currentVersion,
+      packageJson.version,
       releaseType === "stable" ? "major" : "minor"
     );
   }
@@ -41,45 +51,45 @@ if (!newVersion) {
 
 console.log(` change version: ${currentVersion} → ${newVersion}`);
 
-// Write new version to package.json
-packageJson.version = newVersion;
-fs.writeFileSync(pkgPath, JSON.stringify(packageJson, null, 2) + "\n", "utf8");
+// // Write new version to package.json
+// packageJson.version = newVersion;
+// fs.writeFileSync(pkgPath, JSON.stringify(packageJson, null, 2) + "\n", "utf8");
 
-// logs
-let changelog = "";
-const tagName = `v${newVersion}`;
+// // logs
+// let changelog = "";
+// const tagName = `v${newVersion}`;
 
-try {
-  changelog = execSync(
-    `npx conventional-changelog -p angular --tag-prefix "v" --from ${currentVersion}`,
-    { encoding: 'utf-8' }
-  ).trim();
+// try {
+//   changelog = execSync(
+//     `npx conventional-changelog -p angular --tag-prefix "v" --from ${currentVersion}`,
+//     { encoding: 'utf-8' }
+//   ).trim();
 
-  if (!changelog) {
-    changelog = execSync(
-      `npx conventional-changelog -p angular -r 0`,
-      { encoding: 'utf-8' }
-    ).trim();
-  }
-} catch (err) {
-  console.error('❌ Error generating changelog:', err.message);
-  process.exit(1);
-}
-console.log("🚀 ~ changelog:", changelog);
+//   if (!changelog) {
+//     changelog = execSync(
+//       `npx conventional-changelog -p angular -r 0`,
+//       { encoding: 'utf-8' }
+//     ).trim();
+//   }
+// } catch (err) {
+//   console.error('❌ Error generating changelog:', err.message);
+//   process.exit(1);
+// }
+// console.log("🚀 ~ changelog:", changelog);
 
-// Push tag
-const tagMessage = `✨ Release ${tagName}\n\n${changelog}`;
-execSync(`git tag -a ${tagName} -m "${tagMessage}"`, { stdio: "inherit" });
-execSync(`git push origin ${tagName}`, { stdio: "inherit" });
+// // Push tag
+// const tagMessage = `✨ Release ${tagName}\n\n${changelog}`;
+// execSync(`git tag -a ${tagName} -m "${tagMessage}"`, { stdio: "inherit" });
+// execSync(`git push origin ${tagName}`, { stdio: "inherit" });
 
-console.log(` tag ${tagName}.`);
+// console.log(` tag ${tagName}.`);
 
-// release
-try {
-  execSync(`gh release create ${tagName} --title "${tagName}" --notes "${tagMessage.replace(/"/g, '\\"')}"`, {
-    stdio: 'inherit',
-  });
-  console.log(`release tag ${tagName} published.`);
-} catch (err) {
-  console.error("Failed  release:", err.message);
-}
+// // release
+// try {
+//   execSync(`gh release create ${tagName} --title "${tagName}" --notes "${tagMessage.replace(/"/g, '\\"')}"`, {
+//     stdio: 'inherit',
+//   });
+//   console.log(`release tag ${tagName} published.`);
+// } catch (err) {
+//   console.error("Failed  release:", err.message);
+// }
